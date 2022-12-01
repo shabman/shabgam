b4a3067bf0e48685d2b3fff1cfe1ca08967b8a8f
shabman <avia.shabbyman@gmail.com>
2022-12-01 18:16:40 +0000

    Initial Commit

---
+++ Raylib9Jam/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Raylib9Jam
+//
+//  Created by Mustafa Malik on 01/12/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
